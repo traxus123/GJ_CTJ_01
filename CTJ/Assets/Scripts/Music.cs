32ab180e9a89933c6bf2f2165bf47adf07d8dffe
traxus123 <thomas.f.rebouillat@gmail.com>
2019-08-03 20:05:18 +0200

    Gestion Musique

---
+++ CTJ/Assets/Scripts/Music.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Music : MonoBehaviour
+{
+    public AudioClip introMusic;
+    public AudioClip loopMusic;
+    public AudioSource music;
+    // Start is called before the first frame update
+    void Start()
+    {
+        music = GetComponent<AudioSource>();
+        music.clip = introMusic;
+        music.Play(0);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(music.timeSamples);
+        if (!music.isPlaying)
+        {
+            music.clip = loopMusic;
+            music.Play();
+            music.loop = true;
+        }
+    }
+}
