2f12db5a9236817ae28077ffad12b23c6e6808c6
traxus123 <thomas.f.rebouillat@gmail.com>
2019-08-03 11:50:12 +0200

    Follower

---
+++ CTJ/Assets/Scripts/Follower.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follower : MonoBehaviour
+{
+    public Transform Player;
+    public Transform PFollower;
+    public int Time = 300;
+
+    private Queue<Vector3> mouvment = new Queue<Vector3>();
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("m_Grounded " + Player.position);
+        mouvment.Enqueue(Player.position);
+        if(mouvment.Count >= Time){
+            PFollower.position = mouvment.Peek();
+            mouvment.Dequeue();
+        }
+    }
+}
