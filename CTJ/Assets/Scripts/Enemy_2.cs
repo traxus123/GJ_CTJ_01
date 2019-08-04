cb0bf21702f192cfc5abff1fd821133aedd514f8
Ugo <ugo.schiapparelli@gmail.com>
2019-08-04 12:44:43 +0200

    Add particles system to eneme2 / Fix Dyno

--- CTJ/Assets/Scripts/Enemy_2.cs
+++ CTJ/Assets/Scripts/Enemy_2.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class Enemy_2 : Enemy
 {
+    public GameObject particleSystem;
     public Transform player;
     public Transform enemy;
     public float speed = 5.0f;
     public float radius = 5.0f;
 
+    private bool m_facingRight = false;
     private bool mooving = false;
     private bool onCooldown = false;
     private int cooldown;
@@ -26,10 +28,20 @@ public class Enemy_2 : Enemy
             if (Slow)
                 speedX /= 2.0f;
 
+            Vector3 prevPos = transform.position;
+
             transform.position = Vector3.MoveTowards(transform.position, destination, speedX * Time.deltaTime);
-            
+
+            Vector3 isFlipable = prevPos - transform.position;
+
+            if (isFlipable.x > 0.0f && m_facingRight)
+                Flip();
+            else if (isFlipable.x < 0.0f && !m_facingRight)
+                Flip();
+
             if (Vector3.Distance(transform.position, destination) < 1f)
             {
+                particleSystem.SetActive(false);
                 mooving = false;
                 onCooldown = true;
             }
@@ -51,6 +63,7 @@ public class Enemy_2 : Enemy
         if (collision.tag == "Character" && !mooving && !onCooldown)
         {
             destination = collision.transform.position;
+            particleSystem.SetActive(true);
             mooving = true;
         }
 
@@ -58,5 +71,13 @@ public class Enemy_2 : Enemy
         {
             destination = collision.transform.position;
         }
+    }
+
+    private void Flip()
+    {
+        m_facingRight = !m_facingRight;
+        Vector3 invScale = transform.localScale;
+        invScale.x *= -1;
+        transform.localScale = invScale;
     }
 }
