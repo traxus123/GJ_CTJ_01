ebe9a9b35d86bad109be0f506c02047cd63d01a1
Ugo <ugo.schiapparelli@gmail.com>
2019-08-04 20:13:22 +0200

    TItle Screen add

---
+++ CTJ/Assets/Scripts/TitleScreen.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreen : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Credits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+}
